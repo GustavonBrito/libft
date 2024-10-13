b0511a83f0a2909d075ac0e0fddf491fec43aa81
gustavon_brito <gustavoserafio@gmail.com>
2024-10-13 18:10:04 -0300

    to lower implemented

---
+++ ft_tolower.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 18:03:12 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/13 18:03:12 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_tolower(int c)
+{
+    if (c < 65 || c > 90)
+    {
+        return c;
+    }
+    return (c + 32);
+}
+
+int main(void)
+{
+    int i;
+    
+    char gustavo[] = "GUSTAVO";
+    i = 0;
+    while (gustavo[i]) {
+        printf("%c", ft_tolower(gustavo[i]));
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
