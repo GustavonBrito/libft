b2e2e34984fb876590e224b56fea68addcff7221
gustavon_brito <gustavoserafio@gmail.com>
2024-10-13 18:27:15 -0300

    strlen implemented

---
+++ ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 18:11:08 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/13 18:11:08 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t ft_strlen(char *s)
+{
+    size_t s_length;
+
+    s_length = 0;
+    while (*s)
+    {
+        s_length++;
+        s++;
+    }
+    return (s_length);
+}
+
+// int main(void)
+// {
+//     char gustavo[] = "gustavo";
+    
+//     printf("%d", ft_strlen(gustavo));
+//     return (0);
+// }
