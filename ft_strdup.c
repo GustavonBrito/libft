b3c9debf19960ba9f9e932848627474fb69eca1e
GustavonBrito <gustavoserafio@gmail.com>
2024-10-16 01:19:39 -0300

    iniciando exercicio de dup

---
+++ ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 00:51:17 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2024/10/16 01:16:32 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*strdup(const char *s)
+{
+	int		index;
+	char	*string_dup;
+	int		s_size;
+
+	index = 0;
+	s_size = 0;
+	while (s[index])
+	{
+		index++;
+		s_size++;
+	}
+	string_dup = (char *)malloc(s_size + 1 * sizeof(char));
+	index = 0;
+	while (s[index])
+	{
+		string_dup[index] = s[index];
+		index++;
+	}
+	string_dup[index + 1] = '\0';
+	return (string_dup);
+}
+
+int	main(void)
+{
+	char *s = "gustavo";
+	free(strdup(s));
+	printf("%s\n", strdup(s));
+	return (0);
+}
