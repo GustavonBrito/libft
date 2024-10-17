f7c6ae676bc5a2d7da090da3fc1fd4b6857532b8
GustavonBrito <gustavoserafio@gmail.com>
2024-10-17 11:32:51 -0300

    ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,13 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/16 10:23:13 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/16 10:23:13 by marvin           ###   ########.fr       */
+/*   Created: 2024/10/17 11:01:27 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/17 11:01:27 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
+
+static char * do_new_string(int s_size, int last_index, char* s,  char c){
+    int index;
+
+    index = 0;    
+    while (s_size > 0)
+    {
+        if (s[s_size - 1] == c)
+        {
+            while (s_size - 1 < last_index)
+            {
+                s[index] = s[s_size - 1];
+                s_size++;
+                index++;
+            }
+            s[index] = '\0';
+            break;
+        }
+        s_size--;
+    }
+
+    return (s);
+}
 
 char *ft_strrchr(char *s, int c)
 {
@@ -21,6 +44,8 @@ char *ft_strrchr(char *s, int c)
 
     index = 0;
     s_size = 0;
+    if (c == '\0')
+        return ("\0");
     while (s[index])
     {
         s_size++;
@@ -28,30 +53,19 @@ char *ft_strrchr(char *s, int c)
     }
     last_index = s_size;
     index = 0;
-    while (s_size > 0)
-    {
-        if (s[s_size - 1] == c)
-        {
-            while (s_size - 1< last_index)
-            {
-                s = "0";
-                s[index] = s[s_size - 1];
-                s_size++;
-                index++;
-            }
-            break;
-        }
-        s_size--;
-    }
+
+    s = do_new_string(s_size, last_index, s, c);
+
     return (s);
 }
 
 int main(void)
 {
     char hello[] = "Hello, World!";
-    char a = 'o';
+    char a = '\0';
 
 
-    ft_strrchr(hello, a);
+    printf("%s", ft_strrchr(hello, a));
+    //printf("%s", strrchr(hello, a));
     return (0);
 }
