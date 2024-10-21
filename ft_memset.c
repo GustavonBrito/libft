bda5b806773766bce51406bccacb4f14922bc6c8
GustavonBrito <gustavoserafio@gmail.com>
2024-10-21 13:39:03 -0300

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,20 +6,37 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 08:38:36 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2024/10/21 08:41:22 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2024/10/21 13:38:03 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
-// void *ft_memset(void* s, int c, size_t n)
-// {
-// 	unsigned char* s_unsigned;
-// 	unsigned int
-// }
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*s_unsigned;
+	unsigned int	c_unsigned;
+	unsigned int	i;
 
-// int main(void)
+	s_unsigned = s;
+	c_unsigned = c;
+	i = 0;
+	if (n == 0)
+		return (s_unsigned);
+	while (i < n)
+	{
+		s_unsigned[i] = c_unsigned;
+		i++;
+	}
+	return (s_unsigned);
+}
+
+// int	main(void)
 // {
-// 	ft_memset();
-// 	return(0);
+// 	char src[] = "gustavoaaa";
+// 	unsigned char *sla = ft_memset(src, 'g', 15);
+// 	//unsigned char *sla1 = memset(src, 'g', 15);
+// 	printf("%s\n", sla);
+// 	//printf("%s\n", sla1);
+// 	return (0);
 // }
