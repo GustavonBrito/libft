3f7a80a32281bbcb2617ed7060a7d137e04e8729
GustavonBrito <gustavoserafio@gmail.com>
2024-10-21 08:41:35 -0300

    fazendo memset

---
+++ ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 08:38:36 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2024/10/21 08:41:22 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+// void *ft_memset(void* s, int c, size_t n)
+// {
+// 	unsigned char* s_unsigned;
+// 	unsigned int
+// }
+
+// int main(void)
+// {
+// 	ft_memset();
+// 	return(0);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:18:17 by marvin            #+#    #+#             */
-/*   Updated: 2024/10/20 00:58:27 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2024/10/21 08:21:04 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,4 @@ char	*ft_strchr(char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
